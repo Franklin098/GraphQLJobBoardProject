bcb07182eab5b72822a918305eac740d76aca1e6
Franklin Velasquez <iefranklin@hotmail.es>
2022-12-25 17:59:41 -0600

    add multiple resolvers in the Backend. create recursive object graph navigation. add Frontend queries to fetch data from the backend using GraphQL

---
+++ server/resolvers.js
@@ -0,0 +1,28 @@
+import { Job, Company } from "./db.js";
+
+export const resolvers = {
+  Query: {
+    // the resolver func can be asynchronous, which is useful if we want to fetch data from a database
+    jobs: async () => Job.findAll(),
+    job: (_root, args) => {
+      const id = args.id;
+      return Job.findById(id);
+    },
+    company: (_root, { id }) => Company.findById(id),
+  },
+
+  Job: {
+    // first argument is the parent object, in thi case a job
+    company: (job) => {
+      // console.log(`resolving company for job:`, job);
+      return Company.findById(job.companyId);
+    },
+  },
+
+  Company: {
+    jobs: (company) => Job.findAll((job) => job.companyId === company.id),
+  },
+};
+
+// GraphQL has a 'resolution chain', it works like a tree from top to button
+// First it starts at the Query object and we can writing specific resolvers as needed
--- server/server.js
+++ server/server.js
@@ -1,30 +1,49 @@
-import cors from 'cors';
-import express from 'express';
-import { expressjwt } from 'express-jwt';
-import jwt from 'jsonwebtoken';
-import { User } from './db.js';
+import cors from "cors";
+import express from "express";
+import { expressjwt } from "express-jwt";
+import jwt from "jsonwebtoken";
+import { User } from "./db.js";
+import { ApolloServer } from "apollo-server-express";
+import { readFileSync } from "fs";
+import { resolvers } from "./resolvers.js";
 
 const PORT = 9000;
-const JWT_SECRET = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
+const JWT_SECRET = Buffer.from("Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt", "base64");
 
 const app = express();
-app.use(cors(), express.json(), expressjwt({
-  algorithms: ['HS256'],
-  credentialsRequired: false,
-  secret: JWT_SECRET,
-}));
+app.use(
+  cors(),
+  express.json(),
+  expressjwt({
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+    secret: JWT_SECRET,
+  })
+);
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne((user) => user.email === email);
   if (user && user.password === password) {
     const token = jwt.sign({ sub: user.id }, JWT_SECRET);
-    res.json({ token });  
+    res.json({ token });
   } else {
     res.sendStatus(401);
   }
 });
 
+// import the schema definition
+const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+await apolloServer.start();
+
+// we are going to handle graphql request in the /graphql path
+apolloServer.applyMiddleware({ app, path: "/graphql" });
+
+// you can support other Rest and GraphQL request in the same server
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
