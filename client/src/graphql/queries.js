ce33cac25e7226d2eb0787efc920b3833dc6eb61
Franklin Velasquez <iefranklin@hotmail.es>
2022-12-29 08:33:04 -0600

    add graphQL mutation queries, add form functionality to create a new Job

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -56,3 +56,22 @@ export async function getCompany(id) {
   const { company } = await request(GRAPHQL_URL, query, variables);
   return company;
 }
+
+export async function createJob(input) {
+  const query = gql`
+    mutation CreateJoMutation($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        id
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+
+  const variables = { input };
+  const data = await request(GRAPHQL_URL, query, variables);
+  return data.job;
+}
