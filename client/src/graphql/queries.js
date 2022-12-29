2099c0626d1502b0e47c50557b0442cb474f160a
Franklin Velasquez <iefranklin@hotmail.es>
2022-12-29 12:52:39 -0600

    add authorization header in request

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,4 +1,5 @@
 import { request, gql } from "graphql-request";
+import { getAccessToken } from "../auth";
 
 const GRAPHQL_URL = "http://localhost:9000/graphql";
 
@@ -72,6 +73,7 @@ export async function createJob(input) {
   `;
 
   const variables = { input };
-  const data = await request(GRAPHQL_URL, query, variables);
+  const headers = { Authorization: `Bearer ${getAccessToken()}` };
+  const data = await request(GRAPHQL_URL, query, variables, headers);
   return data.job;
 }
